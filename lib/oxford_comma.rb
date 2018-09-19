fc86d89411f5130d780ac8032c625e749bb73da6
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 17:18:24 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,6 +9,7 @@ def oxford_comma(array)
         if (n == 2)
           str += "#{array[2*i..2*i+1].join(" and ")}"
         elsif (2*i+1 == n-1)
+          puts "yeah"
           str += "#{array[2*i..2*i+1].join(", and ")}"
         else
           str += "#{array[2*i..2*i+1].join(", ")}"

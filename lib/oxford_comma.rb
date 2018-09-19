1210b0cfefd596949683676ef40c211915f46575
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-09-19 17:07:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,9 @@ def oxford_comma(array)
   n = array.length
   str = ""
   (n/2).times do |i|
-    if (n == 2)
+    if n==1
+      str += array[0]
+    elsif (n == 2)
       str += array[2*i, 2*i+1].join(" and ")
     elsif (2*i+1 == n-1)
       str += array[2*i, 2*i+1].join(", and ")
